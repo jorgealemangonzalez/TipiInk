de19f061d6757e4c0985b8f76d200649817c68eb
Jorge Aleman gonzalez <jorge.alemangonzalez@gmail.com>
2025-03-22 11:07:27 +0100

    docs: Add frontend docs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
     root: true,
     env: { browser: true, es2020: true },
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
-    ignorePatterns: ['dist', '.eslintrc.cjs', '**/*.css'],
+    ignorePatterns: ['dist', '.eslintrc.cjs', '**/*.css', '**/*.md', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
     rules: {

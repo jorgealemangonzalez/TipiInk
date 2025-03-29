eb1e7d6a771a2a45fec0fde731d0456671450658
Jorge Aleman gonzalez <jorge.alemangonzalez@gmail.com>
2025-03-29 13:00:26 +0100

    Add domains

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,2 +1,5 @@
-export const isTestEnvironment = window.location.hostname !== 'app.tipi.ink'
+export const isTestEnvironment = 
+    window.location.hostname !== 'app.tipi.ink' && 
+    window.location.hostname !== 'app.hitipi.com' &&
+    window.location.hostname !== 'app.oidotipi.com'
 export const isLocalEnvironment = window.location.hostname === 'localhost'
